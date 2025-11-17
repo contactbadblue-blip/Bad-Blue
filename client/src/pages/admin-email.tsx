9d5fc6b17f8c068975947badf43e071982a2b678
Rjdclink <brclink1985@gmail.com>
2025-11-16 23:52:38 -0600

    Update admin-email.tsx

--- client/src/pages/admin-email.tsx
+++ client/src/pages/admin-email.tsx
@@ -460,7 +460,8 @@ export default function AdminEmail() {
                 </p>
               </div>
         </CardContent>
-        </Card>
+        
+      </Card>
         )}
         
        {/* Section B: Send Email to Any Address */}
