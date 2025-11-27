d64509fe51712f3fff78a4edcc24d797cc2206e7
Rjdclink <brclink1985@gmail.com>
2025-11-27 09:07:52 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -184,12 +184,14 @@ app.use((req, res, next) => {
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
-  } else {
-    // Production: serve client SPA directly from /client
-    const clientDir = path.join(__dirname, "..", "client");
+} else {
+    // Production: serve client SPA directly from built dist folder
+-   const clientDir = path.join(__dirname, "..", "client");
++   const clientDir = path.join(__dirname, "..", "client", "dist");
 
-    // Serve static assets from /client (JS, CSS, images, etc.)
-    app.use(express.static(clientDir));
+    // Serve static assets from compiled build
+-   app.use(express.static(clientDir));
++   app.use(express.static(clientDir));
 
     // For any non-API, non-SEO route, send back index.html
     app.get("*", (req: Request, res: Response, next: NextFunction) => {
