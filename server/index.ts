6cc611cee196cc6b99e9d95bfbba0171feb686a1
Rjdclink <brclink1985@gmail.com>
2025-11-27 09:35:19 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -96,14 +96,8 @@ app.use((req, res, next) => {
     res.sendFile("sitemap.xml", { root: "public" });
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+// Always use Vite middleware (Railway runs in production by default)
+  await setupVite(app, server);
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
