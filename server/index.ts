55658618d562d52cabee12be908e9ad678af26cb
Rjdclink <brclink1985@gmail.com>
2025-11-27 11:08:44 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,10 @@ import type { Request, Response } from "express";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 declare module "http" {
