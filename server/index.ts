f7f503ee1dd05cad74f33a95bcfa7cd6e05aef15
Rjdclink <brclink1985@gmail.com>
2025-11-26 20:39:54 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ import { createClient } from "@supabase/supabase-js";
 import express, { type Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite'http'ort;
+import { setupVite, serveStatic, log } from "./vite;
 const app = express();
 app.use(express.json({
   verify: (req, _res, buf) => {
